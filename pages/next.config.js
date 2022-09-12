d163f8055bf701c20c95317da932df4329eae053
Kenji Saito <ken-yo@mbr.nifty.com>
2022-09-12 23:54:01 +0900

    Bumps node modules

--- pages/next.config.js
+++ pages/next.config.js
@@ -1,20 +1,14 @@
-/** @type {import('next').NextConfig} */
 const withPlugins = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
 
+/** @type {import('next').NextConfig} */
 module.exports = withPlugins([
     [withBundleAnalyzer],
 ],
     {
-        webpack5: true,
         reactStrictMode: true,
-        esmExternals: true,
-        swcLoader: true,
         swcMinify: true,
-        experimental: {
-            modern: true,
-        }
     }
 );
