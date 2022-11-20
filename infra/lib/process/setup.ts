c11da00c8166857bd40f2362076c6174420c7a0e
Kenji Saito <ken-yo@mbr.nifty.com>
2022-11-20 14:56:52 +0900

    Replace the S3 access to OAC with CloudFront Functions

--- infra/lib/process/setup.ts
+++ infra/lib/process/setup.ts
@@ -3,25 +3,30 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export const nextJsExport = () => {
-    const nextJsAppRoot = `${process.cwd()}/../pages`;
-    [`${nextJsAppRoot}/.next`, `${nextJsAppRoot}/out`].forEach(dir => {
-        if (fs.existsSync(dir)) {
-            fs.rmdirSync(dir, {
-                recursive: true,
-            });
-        }
-    });
+  const nextJsAppRoot = `${process.cwd()}/../pages`;
+  [`${nextJsAppRoot}/.next`, `${nextJsAppRoot}/out`].forEach((dir) => {
+    if (fs.existsSync(dir)) {
+      fs.rmdirSync(dir, {
+        recursive: true,
+      });
+    }
+  });
 
-    ['yarn build', 'yarn export',].forEach((cmd => {
-        childProcess.execSync(cmd, {
-            cwd: `${nextJsAppRoot}`,
-            stdio: ['ignore', 'inherit', 'inherit'],
-            env: { ...process.env },
-            shell: 'bash'
-        });
-    }));
-    fs.readdirSync(`${nextJsAppRoot}/src/public`)
-        .map(file => path.basename(file))
-        .filter(file => file !== '.DS_Store')
-        .forEach(file => fs.copyFileSync(`${nextJsAppRoot}/src/public/${file}`, `${nextJsAppRoot}/out/${file}`));
+  ['yarn build', 'yarn export'].forEach((cmd) => {
+    childProcess.execSync(cmd, {
+      cwd: `${nextJsAppRoot}`,
+      stdio: ['ignore', 'inherit', 'inherit'],
+      env: { ...process.env },
+      shell: 'bash',
+    });
+  });
+  fs.readdirSync(`${nextJsAppRoot}/src/public`)
+    .map((file) => path.basename(file))
+    .filter((file) => file !== '.DS_Store')
+    .forEach((file) =>
+      fs.copyFileSync(
+        `${nextJsAppRoot}/src/public/${file}`,
+        `${nextJsAppRoot}/out/${file}`,
+      ),
+    );
 };
