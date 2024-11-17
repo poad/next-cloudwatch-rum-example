f172b2f033012cac64ec08bdac7da457c9e9779e
Kenji Saito <ken-yo@mbr.nifty.com>
2024-11-17 21:52:35 +0900

    Bumps node modules

--- pages/eslint.config.mjs
+++ pages/eslint.config.mjs
@@ -74,8 +74,6 @@ export default tseslint.config(
       'arrow-parens': ['error', 'always'],
       semi: ['error', 'always'],
       quotes: ['error', 'single'],
-      '@typescript-eslint/no-unused-expressions': 'off',
-      '@typescript-eslint/no-empty-function': 'off',
     },
   },
 );
